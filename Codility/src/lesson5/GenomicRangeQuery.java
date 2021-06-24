a22103695780f48686d09708a2fa29c3d06d2cf3
dykim14 <dykim14@gmail.com>
2021-06-24 15:17:38 +0900

    Codility - GenomicRangeQuery 완성

--- Codility/src/lesson5/GenomicRangeQuery.java
+++ Codility/src/lesson5/GenomicRangeQuery.java
@@ -11,31 +11,32 @@ public class GenomicRangeQuery {
         int[] G = new int[sLength + 1];
 
         for (int i = 0; i < sLength; i++) {
-            switch (sArray[i]) {
-                case 'A':
-                    A[i]++;
-                    break;
-                case 'C':
-                    C[i]++;
-                    break;
-                case 'G':
-                    G[i]++;
-                    break;
-            }
             A[i+1] = A[i];
             C[i+1] = C[i];
             G[i+1] = G[i];
+            switch (sArray[i]) {
+                case 'A':
+                    A[i+1]++;
+                    break;
+                case 'C':
+                    C[i+1]++;
+                    break;
+                case 'G':
+                    G[i+1]++;
+                    break;
+            }
+
         }
 
-        for (int i = 0; i < sLength; i++) {
+        for (int i = 0; i < sLength + 1; i++) {
             System.out.print(A[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < sLength; i++) {
+        for (int i = 0; i < sLength + 1; i++) {
             System.out.print(C[i] + " ");
         }
         System.out.println();
-        for (int i = 0; i < sLength; i++) {
+        for (int i = 0; i < sLength + 1; i++) {
             System.out.print(G[i] + " ");
         }
         System.out.println();
@@ -43,26 +44,11 @@ public class GenomicRangeQuery {
         int pLength = P.length;
         int[] answer = new int[pLength];
         for (int i = 0; i < pLength; i++) {
-            if (Q[i] == P[i]) {
-                switch (sArray[Q[i]]) {
-                    case 'A':
-                        answer[i] = 1;
-                        break;
-                    case 'C':
-                        answer[i] = 2;
-                        break;
-                    case 'G':
-                        answer[i] = 3;
-                        break;
-                    case 'T':
-                        answer[i] = 4;
-                        break;
-                }
-            } else if (A[Q[i]] - A[P[i]] > 0) {
+            if (A[Q[i] + 1] - A[P[i]] > 0) {
                 answer[i] = 1;
-            } else if (C[Q[i]] - C[P[i]] > 0) {
+            } else if (C[Q[i] + 1] - C[P[i]] > 0) {
                 answer[i] = 2;
-            } else if (G[Q[i]] - G[P[i]] > 0) {
+            } else if (G[Q[i] + 1] - G[P[i]] > 0) {
                 answer[i] = 3;
             } else {
                 answer[i] = 4;
